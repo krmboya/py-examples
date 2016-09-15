357a18633c394a2ad938fda92912b7caa9e4f81d
Kenny Rachuonyo <krmboya@gmail.com>
2016-09-15 15:30:57 +0300

    began quicksort

---
+++ quicksort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Quicksort in python using the Lomuto partition scheme
+"""
+import random
+
+# We have a list of unsorted integers
+l = [random.randint(1, 100) for i in range(10)]
+
+print l
