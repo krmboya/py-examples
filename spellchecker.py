c5a250c1877a60e08b751e435eb72e5227bf2ffe
Kenny Rachuonyo <krmboya@gmail.com>
2016-11-19 15:26:59 +0300

    complete spellchecker

--- spellchecker.py
+++ spellchecker.py
@@ -25,12 +25,17 @@ def correction(word):
 
 def candidates(word):
     "Generate possible corrections for `word`"
+    # Candidates are:
+    # the word itself it appears in dictionary or
+    # words 1 edit away if they appear in dictionary or
+    # words 2 edits away if they appear in dictionary or
+    # the word itself if none of the above check out
     return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
 
 def known(words):
     """Returns subset of `words` that appear in dictionary WORDS"""
-    return set(w for w in words in w in WORDS)
+    return set(w for w in words if w in WORDS)
 
 
 def edits1(word):  # probably most interesting part
@@ -49,8 +54,27 @@ def edits1(word):  # probably most interesting part
     # ['apple', 'pple', 'plep', lepp']
     deletes = [L + R[1:] for L, R in splits if R]
 
-    transposes = # will continue here..
+    # Transpose the first, second character of the second word in each
+    # tuple e.g. ['plepa', 'lepap']
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+
+    # Replace the 1st character of the second word with each
+    # letter of the alphabet e.g. 
+    # ['pplea', 'ppleb', 'pplec' ... 'ppleap', 'pplebp', 'pplecp' .. etc
     
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+
+    # Insert a letter of the alphabet between words in each tuple
+    # e.g. ['ppleaa', 'ppleba', ... 'pleaap', 'pplebap' ...
+    inserts = [L + c + R for L, R in splits for c in letters]
+
+    # and now rid all the duplicates among the above lists
+    return set(deletes, transposes, replaces, inserts)
+
+
+def edits2(word):
+    """All edits that are 2 edits away from `word`"""
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
 
 
